529cab6d868c4ac360d41c2775e4732b15b89369
dinary-upos <max.orel.site@yandex.kz>
2025-03-15 18:57:35 +0300

    response not only on A requests

--- dns_server.py
+++ dns_server.py
@@ -1,5 +1,7 @@
 import asyncio
 import concurrent.futures
+import socket
+
 from shared_storage import SharedStorage
 
 class PseudoDNSServer(asyncio.DatagramProtocol):
@@ -54,26 +56,35 @@ class PseudoDNSServer(asyncio.DatagramProtocol):
         # them splitted - last byte of response is pos+4, first byte of
         # additional sections (garbage) is pos+5, due to slices in python
         # both times we should pass pos+5
-        response = self.build_response(data, ip, pos+5)
+        response = self.build_response(data, ip, pos+5, hostname) # hostname is only for debug
         self.transport.sendto(response, addr)
 
-    def build_response(self, request: bytes, ip: bytes, garbage_start: int) -> bytes:
+    def build_response(self, request: bytes, ip: bytes, garbage_start: int, hostname: bytes) -> bytes:
         # Construct the DNS response header
         # \x81\x80: Standard DNS response flags (QR=1, Opcode=0, AA=1, TC=0, RD=1, RA=1, Z=0, RCODE=0)
         header = request[:2] + b'\x81\x80'  # Response flags
-        header += request[4:6] + request[4:6] + request[8:12] # count of sections (same as in the request)
         question = request[12:garbage_start]
 
-        # Construct the DNS answer section
-        # \xc0\x0c: Pointer to the domain name in the question (mark '11', instead of copying)
-        # \x00\x01\x00\x01: Type A (host address) and Class IN (Internet) - last four bytes in the question
-        # \x00\x00\x00\x3c: TTL (60 seconds)
-        # \x00\x04: Length of the IP address (4 bytes)
-        type_class = question[-4:]
-        answer = b'\xc0\x0c' + type_class + b'\x00\x00\x00\x0f\x00\x04' + ip
+        type_int = int.from_bytes(question[-4:-2], 'big')
+        print(f'DEBUG: type_int={type_int}')
+        # HOOK: we're supporting only IPv4 (1), so AAAA (28) and other
+        # are dropped, but if we don't return anything or return response for A
+        # client programs dropping it, so we should return empty answer on non-A
+        if type_int != 1:
+            header += request[4:6] + b'\x00\x00' + request[8:12] # count of sections
+            answer = b''
+        else:
+            header += request[4:6] + request[4:6] + request[8:12]
+            # Construct the DNS answer section
+            # \xc0\x0c: Pointer to the domain name in the question (mark '11', instead of copying)
+            # \x00\x01\x00\x01: Type A (host address) and Class IN (Internet) - last four bytes in the question
+            # \x00\x00\x00\x3c: TTL (60 seconds)
+            # \x00\x04: Length of the IP address (4 bytes)
+            type_class = question[-4:]
+            answer = b'\xc0\x0c' + type_class + b'\x00\x00\x00\x0f\x00\x04' + ip
 
         result = header + question + answer + request[garbage_start:]
-        print(f'DEBUG: request {request.hex()}, response {result.hex()}')
+        print(f'DEBUG: request {request.hex()}, response {result.hex()}, resolving {hostname.decode()} to {socket.inet_ntoa(ip)}')
         return result
 
 
